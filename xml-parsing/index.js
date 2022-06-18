7f9616ff3ba7eda47d18a069366fd21578320470
danjiehu <danjiehu@outlook.com>
2022-06-18 18:03:14 +0800

    added reference and export to csv function

--- xml-parsing/index.js
+++ xml-parsing/index.js
@@ -1,3 +1,4 @@
+// ref: https://www.youtube.com/watch?v=VxKGVb0oOBw
 let xmlContent = ""; // create an empty xml 
 let tableBooks = document.querySelector("#books"); // tableBooks is the table with id books in the html file
 
@@ -45,4 +46,47 @@ fetch("books.xml").then(res => { // getting xml content from "books.xml"
             tableBooks.children[1].appendChild(row);
         })
     })
+})
+
+// ref: https://yourblogcoach.com/export-html-table-to-csv-using-javascript/
+// function def: html to csv
+function htmlToCSV(html, filename) {
+    let data = [];
+    let rows = document.querySelectorAll("tr");
+
+    for (let i = 0; i < rows.length; i++) {
+        let row = [], cols = rows[i].querySelectorAll("td, th");
+
+        for (let j = 0; j < cols.length; j++) {
+            row.push(cols[j].innerText);
+        }
+
+        data.push(row.join(","));
+    }
+
+    downloadCSVFile(data.join("\n"), filename);
+}
+
+// function def: downloadCSVFile
+function downloadCSVFile(csv, filename) {
+    var csv_file, download_link;
+
+    csv_file = new Blob([csv], { type: "text/csv" });
+
+    download_link = document.createElement("a");
+
+    download_link.download = filename;
+
+    download_link.href = window.URL.createObjectURL(csv_file);
+
+    download_link.style.display = "none";
+
+    document.body.appendChild(download_link);
+
+    download_link.click();
+}
+
+document.getElementById("download-button").addEventListener("click", function () {
+    var html = document.querySelector("table").outerHTML;
+    htmlToCSV(html, "students.csv");
 })
