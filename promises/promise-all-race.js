ddc10a2853842b352ae5deb311593615ce4d965e
danjiehu <danjiehu@outlook.com>
2022-07-28 09:18:01 +0800

    added demo for Promise.all and Promise.race

---
+++ promises/promise-all-race.js
@@ -0,0 +1,4 @@
+// demostrating the use of Promise.all and Promise.race
+// Promise.all run all three promises at the same time, and resolve when all three are resolved
+// Promise.race run all three promises at the same time, and resolve when the fast one is resolved
+
