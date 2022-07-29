c9477fda374445a9bf9ec89d10780f56abfb4789
danjiehu <danjiehu@outlook.com>
2022-07-29 17:06:05 +0800

    set up debugging

---
+++ json-functions/index.js
@@ -0,0 +1,7 @@
+let fs = require('fs');
+
+const demo = fs.readFileSync('/Users/danjiehu/code/js-playground/json-functions/demo.json', 'utf-8');
+// console.log(demo);
+
+console.log(demo.sanctionEntity);
+console.log(demo.sanctionEntity);
