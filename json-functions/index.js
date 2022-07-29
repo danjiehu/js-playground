df7257a8dc268afa82564ae3d4d9a40e14be041a
danjiehu <danjiehu@outlook.com>
2022-07-29 17:48:06 +0800

    completed json demo

--- json-functions/index.js
+++ json-functions/index.js
@@ -1,7 +1,18 @@
+//ref: 
+
 let fs = require('fs');
 
+// step-1: reading json file 
 const demo = fs.readFileSync('/Users/danjiehu/code/js-playground/json-functions/demo.json', 'utf-8');
-// console.log(demo);
+console.log("type of demo: ", typeof demo); // output: string
+// ! demo is a string
 
-console.log(demo.sanctionEntity);
-console.log(demo.sanctionEntity);
+// step-2: converting demo(string type) into javascript object 
+const demoObj = JSON.parse(demo);
+console.log("demoObj: ", demoObj);
+console.log("type of sanctionEntity: ", typeof demoObj.export.sanctionEntity);
+console.log("sanctionEntity: ", demoObj.export.sanctionEntity[0]);
+demoObj.export.sanctionEntity.forEach(element => {
+    console.log(element.euReferenceNumber);
+});
+
