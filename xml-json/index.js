64e8cb86a823742a9ab9e0d7afa168e347587e00
danjiehu <danjiehu@outlook.com>
2022-07-29 15:56:33 +0800

    added write json file

--- xml-json/index.js
+++ xml-json/index.js
@@ -1,5 +1,19 @@
-let convert = require('xml-js');
-let xml = require('fs').readFileSync('example-1.xml', 'utf-8')
+// trying out xml-js and xml packages 
+// ref: https://nodejs.dev/learn/the-nodejs-fs-module
 
-let result = convert.xml2json(xml, { compact: true, spaces: 4 });
-console.log(result);
+// xml-js ref: https://www.npmjs.com/package/xml-js // npm install xml-js
+// xml2json ref: https://www.npmjs.com/package/xml2json // npm install xml2json
+
+const fs = require('fs');
+let parserOne = require('xml-js'); // xml-js parser 
+let xml = require('fs').readFileSync('/Users/danjiehu/code/js-playground/xml-json/example.xml', 'utf-8');
+let resultOne = parserOne.xml2json(xml, { compact: true, spaces: 4 });
+fs.writeFileSync('/Users/danjiehu/code/js-playground/xml-json/xml-js-result.json', resultOne);
+// console.log("resultOne: ", resultOne);
+
+let parserTwo = require('xml2json');
+let resultTwo = parserTwo.toJson(xml);
+fs.writeFileSync('/Users/danjiehu/code/js-playground/xml-json/xml2json-result.json', resultTwo);
+// console.log("resultTwo: ", resultTwo);
+
+// put result to browser for easier reading
